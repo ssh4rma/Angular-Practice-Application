20711880d276f93bf272bd0bdfbd27e981998d1f
Shubham <shubham.sharma4390@gmail.com>
2025-06-09 08:05:11 +0530

    [Shubham]Add: Implemented Dependency Injection

--- src/app/component/c3/c3.component.ts
+++ src/app/component/c3/c3.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { UserDataService } from 'src/app/services/user-data.service';
 @Component({
   selector: 'app-c3',
   templateUrl: './c3.component.html',
-  styleUrls: ['./c3.component.scss']
+  styleUrls: ['./c3.component.scss'],
 })
 export class C3Component {
+  constructor(private sharedData: UserDataService) {}
 
+  trigger(): void {
+    this.sharedData.getVal();
+    this.sharedData.setVal(100);
+    this.sharedData.getVal();
+  }
 }
